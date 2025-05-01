4910233b9241b2bf727bf1821d1c25d3e9c5f631
alipkin <aleks.lipckin@gmail.com>
2025-05-01 19:50:04 +0500

    fix: install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
-docker network create task-network
+#!/bin/bash
 
-(cd ./Backend/TaskRestApiService && docker compose up -d --build) &
+docker network inspect task-network >/dev/null 2>&1 || docker network create task-network
+
+(cd ./Backend/TaskRestApiService && docker compose up -d --build)
+
+echo "Waiting start task-rest-api-service..."
+until curl -s http://localhost:5437/health >/dev/null; do
+  sleep 1
+done
+echo "task-rest-api-service запущен!"
+
 (cd ./Backend/AuthService && docker compose up -d --build) &
 (cd ./Backend/TaskStorageService && docker compose up -d --build) &
 (cd ./Backend/NotifyService && docker compose up -d --build) &
